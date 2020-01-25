e41ead1379dc712da6fe164d14878e6c2d1c95b9
adimouch21 <adimouch21@gmail.com>
2020-01-25 12:38:15 +0100

    first commit heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   expirationTime : 5000,
-  backend: 'http://localhost:8080' 
+  /* backend: 'http://localhost:8080' */ 
+  backend: 'https://app-atout.herokuapp.com/'
 };
