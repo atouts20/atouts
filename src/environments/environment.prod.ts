db80bf0465e27daff49a72879ca5111f411201ac
adimouch21 <adimouch21@gmail.com>
2020-01-25 14:05:59 +0100

    first commit 25-01-2020

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   expirationTime : 5000,
   /* backend: 'http://localhost:8080' */ 
-  backend: 'https://app-atout.herokuapp.com/'
+  backend: 'https://app-atout.herokuapp.com'
 };
